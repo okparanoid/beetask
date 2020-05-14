95a9d041bd29f8f4fddea5e3457210764ce1b7b9
okparanoid <60581581+okparanoid@users.noreply.github.com>
2020-05-14 20:43:39 +0300

    Update hw.js

--- JS 1/hw-3/hw.js
+++ JS 1/hw-3/hw.js
@@ -6,14 +6,14 @@
     следуя рекомендациям по именованию переменных
 */
 
-let age = 20;
-let text = `Ваш возраст: ${age} лет`;
+const age = 20;
+const text = `Ваш возраст: ${age} лет`;
 alert(text); // Должен вывести: Ваш возраст: 20 лет
 
 
 let hours = 12;
-let minutes = 45;
-hours = hours + 1;
-let textwithtime = `Текущее время: ${hours} : ${minutes}`;
+const minutes = 45;
+hours++;
+const textwithtime = `Текущее время: ${hours} : ${minutes}`;
 
 alert(textwithtime); // Должен вывести: Текущее время: 13:45
