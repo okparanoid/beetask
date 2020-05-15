3344f6b97d09b7d1b70535f3ba29f0f00fae18f4
okparanoid <60581581+okparanoid@users.noreply.github.com>
2020-05-16 02:23:31 +0300

    clear

--- JS 1/hw-1/hw.js
+++
@@ -1,8 +0,0 @@
-// Создать две переменные: name, admin
-// В переменную name запишите своё имя
-// В переменную admin запишите значение переменной name
-// выведите на экран admin, через функцию alert
-
-const name = 'Olga';
-const admin = name;
-alert(admin);
