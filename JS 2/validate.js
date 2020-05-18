c6b2593947b4e6060897756194cf739c763e31c0
okparanoid <60581581+okparanoid@users.noreply.github.com>
2020-05-18 11:15:34 +0300

    Update validate.js

--- JS 2/validate.js
+++ JS 2/validate.js
@@ -1,14 +1,16 @@
 function validate(data) {
     const { login, password, confirmPassword, license, firstName, gender } = data;
 
-    if (!login || login.trim() == '' || !password) {
+    if (login.trim() == '' || !password) {
         alert('Укажите логин/пароль');
     } else if (login === 'beeline' || login === 'beeinterns' || login === 'bee') {
         alert('Логин занят');
     } else if (password.length < 6) {
         alert('Пароль должен быть длинной не менее 6 символов');
     } else if (password !== confirmPassword) {
-        alert('Пароли должны совпадать');
+        alert('Пароли должны совпадать'); 
+    } else if (!firstName) {
+        alert('Введите имя');
     } else if (!license) {
         alert('Необходимо согласие');
     } else { 
